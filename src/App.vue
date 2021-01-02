e734bab958b8641df58b8362bd7e567935f66684
Hugh Wang <18501667737@163.com>
2021-01-02 22:33:12 +0800

    by bash

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="docker">
+    <span class="docker__item docker__item--active">
+      <div class="iconfont">&#xe607;</div>
+      <div class="docker__title">首页</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe63b;</div>
+      <div class="docker__title">购物车</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe6e4;</div>
+      <div class="docker__title">订单</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">我的</div>
+    </span>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  position: absolute;
+  display: flex;
+  padding: 0  .18rem;
+  box-sizing: border-box;
+  //background-color: antiquewhite;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.45rem;
+  border-top: 1px solid gray;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .2rem;
     }
+    &--active {
+      color: deepskyblue;
+    }
+  }
+  &__title {
+    font-size: .2rem;
+    //color: #42b983;
+    transform: scale(0.5 , .5);
+    transform-origin: center top;
   }
 }
 </style>
